2824789e1264f848ac40df6083d587c7df4198b5
LJJordan124 <jordan3313.lj@gmail.com>
2023-08-21 11:43:44 -0400

    Tag model update

--- models/Tag.js
+++ models/Tag.js
@@ -6,15 +6,16 @@ class Tag extends Model {}
 
 Tag.init(
   {
+    // define columns
     id: {
       type: DataTypes.INTEGER,
-      allowNull: false,
       primaryKey: true,
-      autoIncrement: true
+      allowNull: false,
+      autoIncrement: true,
     },
     tag_name: {
-      type: DataTypes.STRING
-    }
+      type: DataTypes.STRING,
+    },
   },
   {
     sequelize,
